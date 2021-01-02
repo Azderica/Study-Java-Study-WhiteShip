3e68155f74e829510174c161978d750e81412abf
Azderica <mh97888@gmail.com>
2021-01-02 22:41:31 +0900

    fix some error

--- src/main/java/week4/GithubLiveStudy.java
+++ src/main/java/week4/GithubLiveStudy.java
@@ -20,26 +20,19 @@ public class GithubLiveStudy {
     }
 
     private void run() throws IOException {
-        // 연결
         connectGitApi();
 
         GHRepository ghRepository = github.getRepository("whiteship/live-study");
-        // 참여자 이름, 참여 횟수
         Map<String, Integer> participant = new HashMap<>();
-        // 모든 이슈를 가져온다.
         List<GHIssue> issues = ghRepository.getIssues(GHIssueState.ALL);
 
-        //Find Issue
         for (GHIssue issue : issues) {
-            // 각각의 Issue 에 존재하는 comment 들을 저장.
             List<GHIssueComment> commentList = issue.getComments();
 
-            // 혹시 모를 유저 중복을 제거하기 위한 Set
             Set<String> nameList = new HashSet<>();
 
             addParticipantInSet(commentList, nameList);
 
-            // 참여자 명단에서 비교한다.
             for (String s : nameList) {
                 hasParticipantInSet(participant, s);
             }
@@ -65,7 +58,7 @@ public class GithubLiveStudy {
     private void printParticipantRate(Map<String, Integer> participant) {
         participant.forEach((key, value) -> {
             double percent = (double) (value * 100) / 18;
-            System.out.printf(key + "  :  " + "%.2f", percent+ "%");
+            System.out.printf(key + "  :  " + "%.2f" +  percent+ "%");
         });
     }
 
