0aa185a108771f1b5a98de070aa3356e25274fb0
Azderica <mh97888@gmail.com>
2021-02-28 23:02:26 +0900

    add week14 code

---
+++ src/main/java/week14/Data.java
@@ -0,0 +1,13 @@
+package week14;
+
+class Data<T> {
+    T data;
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/week14/Main.java
@@ -0,0 +1,9 @@
+package week14;
+
+public class Main {
+    public static void main(String[] args) {
+        Data<String> data = new Data<>();
+        data.setData("New String data");
+        System.out.println(data.getData());
+    }
+}
